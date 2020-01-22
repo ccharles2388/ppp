9490370988d23755d8e591b722525c35cff183d0
Curtis Charles <ccharles2388@gmail.com>
2020-01-21 19:19:51 -0500

    Ignored Client Build To Deploy To Heroku

---
+++ client/build/precache-manifest.776425c5d77068cf2cae77db06696431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b323e11ff9ea664362e2d70df89985bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca86011776d6d995b928",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "104cf82c7fbcf999ea3e",
+    "url": "/static/js/2.0afe443a.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.0afe443a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca86011776d6d995b928",
+    "url": "/static/js/main.cbd1b39c.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
